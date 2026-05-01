b0386b8e8dd3cae64e6b4ea47a830c7ce430899d
unknown <mehradhetfield@gmail.com>
2026-05-01 09:44:48 +0330

    chore: seed script completed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,6 +24,33 @@ const run = async () => {
       });
     }),
   );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+    },
+  });
+
+  const songs = prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: (await songs).map((song) => ({ id: song.id })),
+          },
+        },
+      });
+    }),
+  );
 };
 
 run()

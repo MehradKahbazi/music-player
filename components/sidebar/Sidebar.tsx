1d027b03adc325f56e1e1dbe5fc8cde3acd8e121
unknown <mehradhetfield@gmail.com>
2026-05-01 05:19:50 +0330

    chore: sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,12 +1,13 @@
 import NextImage from "next/image";
+import NextLink from "next/link";
+import { As } from "@chakra-ui/react";
 import {
   Box,
-  ListItem,
-  ListIcon,
   Divider,
-  Center,
   LinkBox,
   LinkOverlay,
+  List,
+  ListItem,
 } from "@chakra-ui/layout";
 import {
   MdHome,
@@ -15,6 +16,26 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md";
+import CustomMenuItem from "./CustomMenuItem";
+
+// export interface NavItem {
+//   name: string;
+//   route: string;
+//   icon: As; // This tells Chakra "This is a component we can use in the 'as' prop"
+// }
+
+const navMenu = [
+  { name: "Home", icon: MdHome, route: "/" },
+  { name: "Search", icon: MdSearch, route: "/search" },
+  { name: "Your Library", icon: MdLibraryMusic, route: "/library" },
+];
+
+const musicMenu = [
+  { name: "Create Playlist", icon: MdPlaylistAdd, route: "/" },
+  { name: "Favorites", icon: MdFavorite, route: "/favorites" },
+];
+
+const playListz = new Array(30).fill(1).map((_, i) => `playlist ${i + 1}`);
 
 const Sidebar = () => {
   return (
@@ -25,9 +46,37 @@ const Sidebar = () => {
       bg="black"
       color="grey"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
-            <NextImage src="/logo.svg" height={60} width={120} />
+          <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box>
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <CustomMenuItem menu={menu} key={menu.name} />
+            ))}
+          </List>
+        </Box>
+        <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <CustomMenuItem menu={menu} key={menu.name} />
+            ))}
+          </List>
+        </Box>
+        <Divider marginY="20px" color="grey.700" />
+        <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playListz.map((playList) => (
+              <ListItem paddingX="20px" key={playList}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playList}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>

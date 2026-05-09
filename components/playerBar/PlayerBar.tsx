ff08dbbf498b577f4878227f800b9f001a5f128b
unknown <mehradhetfield@gmail.com>
2026-05-09 16:35:34 +0330

    chore: album art added

--- components/playerBar/PlayerBar.tsx
+++ components/playerBar/PlayerBar.tsx
@@ -1,3 +1,4 @@
+import NextImage from "next/image";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import Player from "../player/Player";
 import { useStoreState } from "easy-peasy";
@@ -10,10 +11,20 @@ const PlayerBar = () => {
     <Box height="100px" width="100vw" bg="grey.900" padding="10px">
       <Flex align="center">
         {activeSong ? (
-          <Box padding="20px" color="white" width="30%">
-            <Text fontSize="large">{activeSong.name}</Text>
-            <Text fontSize="sm">{activeSong.artist.name}</Text>
-          </Box>
+          <Flex width="30%" align="center">
+            <Box>
+              <NextImage
+                width="60px"
+                height="60px"
+                unoptimized={true}
+                src={`/api/images/${activeSong?.coverArtUrl}`}
+              />
+            </Box>
+            <Box padding="20px" color="white">
+              <Text fontSize="large">{activeSong.name}</Text>
+              <Text fontSize="sm">{activeSong.artist.name}</Text>
+            </Box>
+          </Flex>
         ) : null}
         <Box width="40%" color="white">
           {activeSong ? <Player songs={songs} activeSong={activeSong} /> : null}

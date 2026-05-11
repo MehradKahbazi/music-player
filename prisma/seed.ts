df5fc20c006c313502cc8b5178b371f0bbc64546
unknown <mehradhetfield@gmail.com>
2026-05-11 17:53:15 +0330

    fix: seed process

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,23 +7,80 @@ const prisma = new PrismaClient();
 
 const run = async () => {
   await Promise.all(
-    artistsData.map(async (artist) => {
-      return prisma.artist.upsert({
-        where: { name: artist.name },
+    artistsData.map(async (artistData) => {
+      // 1. First, ensure the Artist exists and get their ID
+      const artist = await prisma.artist.upsert({
+        where: { name: artistData.name },
         update: {},
-        create: {
-          name: artist.name,
-          songs: {
-            create: artist.songs.map((song) => ({
+        create: { name: artistData.name },
+      });
+
+      // 2. Create a "Default Album" for this artist
+      // We use connectOrCreate so we don't create duplicate albums
+      // if the seed runs twice.
+      const album = await prisma.album.create({
+        // where: {
+        //   // This requires 'name' to be @unique in your Album model
+        //   name: `${artistData.name} Collection`
+        // },
+        // update: {},
+        data: {
+          name: `${artistData.name} Collection`,
+          genre: "Various",
+          artistId: artist.id, // We now have the artist ID!
+        },
+      });
+
+      // 3. Create the Songs and link them to both the Artist and the Album
+      // We use Promise.all here to create all songs for this artist
+      return Promise.all(
+        artistData.songs.map((song) =>
+          prisma.song.create({
+            data: {
               name: song.name,
               duration: song.duration,
               url: song.url,
-            })),
-          },
-        },
-      });
+              artistId: artist.id, // Link to Artist
+              albumId: album.id, // Link to Album
+            },
+          }),
+        ),
+      );
     }),
   );
+  // const unknownArtist = await prisma.artist.upsert({
+  //   where: { name: "Unknown Artist" },
+  //   update: {},
+  //   create: { name: "Unknown Artist" },
+  // });
+
+  // const defaultAlbum = await prisma.album.create({
+  //   where: { name: "Single Tracks" },
+  //   update: {},
+  //   create: {
+  //     name: "Single Tracks",
+  //     artistId: unknownArtist.id,
+  //   },
+  // });
+  // await Promise.all(
+  //   artistsData.map(async (artist) => {
+  //     return prisma.artist.upsert({
+  //       where: { name: artist.name },
+  //       update: {},
+  //       create: {
+  //         name: artist.name,
+  //         songs: {
+  //           create: artist.songs.map((song) => ({
+  //             name: song.name,
+  //             duration: song.duration,
+  //             url: song.url,
+  //             album: defaultAlbum,
+  //           })),
+  //         },
+  //       },
+  //     });
+  //   }),
+  // );
 
   const salt = bcrypt.genSaltSync();
   const user = await prisma.user.upsert({
@@ -37,15 +94,22 @@ const run = async () => {
     },
   });
 
-  const songs = prisma.song.findMany({});
-  await prisma.playlist.create({
-    data: {
-      name: `Cloud Playlist`,
+  const songs = await prisma.song.findMany({});
+  // 4. Seed Playlist
+  await prisma.playlist.upsert({
+    where: {
+      // Note: You might need a unique constraint on Playlist name or ID
+      // For now, we'll use a simple create if you don't have a unique name
+      id: 1,
+    },
+    update: {},
+    create: {
+      name: "Cloud Playlist",
       user: {
         connect: { id: user.id },
       },
       songs: {
-        connect: (await songs).map((song) => ({ id: song.id })),
+        connect: songs.map((song) => ({ id: song.id })),
       },
     },
   });
@@ -57,6 +121,59 @@ const run = async () => {
       },
     },
   });
+
+  // 5. Seed Various Artist and Album
+  // Changed to upsert so it doesn't crash on the second run
+  // await prisma.artist.upsert({
+  //   where: { name: "Various Artist" },
+  //   update: {},
+  //   create: {
+  //     name: "Various Artist",
+  //     albums: {
+  //       create: {
+  //         name: "Cloud Music",
+  //         genre: "Unknown",
+  //         songs: {
+  //           connect: songs.map((song) => ({ id: song.id })),
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+  // await prisma.playlist.create({
+  //   data: {
+  //     name: `Cloud Playlist`,
+  //     user: {
+  //       connect: { id: user.id },
+  //     },
+  //     songs: {
+  //       connect: (await songs).map((song) => ({ id: song.id })),
+  //     },
+  //   },
+  // });
+
+  // const artist = await prisma.artist.create({
+  //   data: {
+  //     name: "Various Artist",
+  //     albums: {
+  //       create: {
+  //         name: "Cloud Music",
+  //         genre: "Unknown",
+  //         songs:{
+  //           connect: (await songs).map((song) => ({ id: song.id })),
+  //         }
+  //       },
+  //     },
+  //   },
+  // });
+  // const album = await prisma.album.create({
+  //   data:{
+  //     ,
+
+  //   }
+  // });
+
+  // })
   // await Promise.all(
   //   new Array(10).fill(1).map(async (_, i) => {
   //     return prisma.playlist.create({

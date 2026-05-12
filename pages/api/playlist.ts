a219ed354c4ec9292189c7c3d678754ed04a14eb
unknown <mehradhetfield@gmail.com>
2026-05-13 00:25:15 +0330

    refactor: api allowed method

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -1,15 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 import { validateRoute } from "../../lib/auth";
 
-export default validateRoute(async (req, res, user) => {
-  const playlists = await prisma.playlist.findMany({
-    where: {
-      userId: user.id,
-    },
-    orderBy:{
-        name: 'asc',
-    }
-  });
+export default validateRoute(
+  async (req: NextApiRequest, res: NextApiResponse, user) => {
+    if (req.method === "GET") {
+      const playlists = await prisma.playlist.findMany({
+        where: {
+          userId: user.id,
+        },
+        orderBy: {
+          name: "asc",
+        },
+      });
 
-  res.json(playlists);
-});
+      return res.json(playlists);
+    }
+    return res.status(405).json({ message: "Method not allowed" });
+  },
+);

a515034a3172eca7e1c8335ff4cab3c9eb546956
unknown <mehradhetfield@gmail.com>
2026-05-13 00:24:00 +0330

    refactor: api allowed method

--- pages/api/music/[name].ts
+++ pages/api/music/[name].ts
@@ -13,34 +13,37 @@ export const config = {
 
 export default validateRoute(
   async (req: NextApiRequest, res: NextApiResponse) => {
-    const { name } = req.query;
+    if (req.method === "GET") {
+      const { name } = req.query;
 
-    const filePath = path.join(
-      process.cwd(),
-      "music-library",
-      "musics",
-      `${name}.mp3`,
-    );
+      const filePath = path.join(
+        process.cwd(),
+        "music-library",
+        "musics",
+        `${name}.mp3`,
+      );
 
+      if (!fs.existsSync(filePath)) {
+        return res.status(404).send("Not Found");
+      }
 
-    if (!fs.existsSync(filePath)) {
-      return res.status(404).send("Not Found");
+      const stat = fs.statSync(filePath);
+
+      res.writeHead(200, {
+        "Content-Type": "audio/mpeg",
+        "Content-Length": stat.size,
+        "Accept-Ranges": "bytes",
+      });
+
+      const readStream = fs.createReadStream(filePath);
+
+      readStream.on("error", (error) => {
+        console.error(error);
+        res.end();
+      });
+      readStream.pipe(res);
+    } else {
+      return res.status(405).json({ message: "Method not allowed" });
     }
-
-    const stat = fs.statSync(filePath);
-
-    res.writeHead(200, {
-      "Content-Type": "audio/mpeg",
-      "Content-Length": stat.size,
-      "Accept-Ranges": "bytes",
-    });
-
-    const readStream = fs.createReadStream(filePath);
-
-    readStream.on("error", (error) => {
-      console.error(error);
-      res.end();
-    });
-    readStream.pipe(res);
   },
 );

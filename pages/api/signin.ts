2fb8dcb1191a1cfa490d5541a9444482e1397626
unknown <mehradhetfield@gmail.com>
2026-05-13 00:26:18 +0330

    refactor: api allowed method/ jwtSign

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,56 +1,47 @@
 /* eslint-disable no-shadow */
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import prisma from "../../lib/prisma";
+import { signJwt } from "../../lib/auth";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body;
-  let user;
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    let user;
 
-  try {
-    user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const { password, ...userWithoutPassword } = user;
-      const token = jwt.sign(
-        {
-          email: user.email,
-          id: user.id,
-          time: Date.now(),
+    try {
+      user = await prisma.user.findUnique({
+        where: {
+          email,
         },
-        "hello",
-        {
-          expiresIn: "8h",
-        },
-      );
-
-      res.setHeader(
-        "Set-Cookie",
-        cookie.serialize("ACCESS_TOKEN", token, {
-          httpOnly: true,
-          maxAge: 8 * 60 * 60,
-          path: "/",
-          sameSite: "lax",
-          secure: process.env.NODE_ENV === "production",
-        }),
-      );
-      console.log(userWithoutPassword);
-
-      res.json({
-        user: userWithoutPassword,
       });
-    } else {
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const { password, ...userWithoutPassword } = user;
+        const token = signJwt(user)
+
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("ACCESS_TOKEN", token, {
+            httpOnly: true,
+            maxAge: 8 * 60 * 60,
+            path: "/",
+            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+          }),
+        );
+
+        return res.json({
+          user: userWithoutPassword,
+        });
+      }
       res.status(401);
-      res.json({ error: "Unauthorized user" });
+      return res.json({ error: "Unauthorized user" });
+    } catch (e) {
+      res.status(401);
+      return res.json({ error: "Unauthorized user" });
     }
-  } catch (e) {
-    res.status(401);
-    res.json({ error: "Unauthorized user" });
   }
+  return res.status(405).json({ message: "Method not allowed" });
 };
